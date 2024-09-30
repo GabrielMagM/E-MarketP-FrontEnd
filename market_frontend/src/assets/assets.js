cc457389436346c118865f511e751d5e4240930b
Gabriel_MagMed <gabriel.t.13@hotmail.com>
2024-09-29 19:23:56 -0500

    we create Hero, and Creating ShopContext to use in Home

--- market_frontend/src/assets/assets.js
+++ market_frontend/src/assets/assets.js
@@ -73,6 +73,7 @@ import contact_img from './contact_img.png'
 import razorpay_logo from './razorpay_logo.png'
 import stripe_logo from './stripe_logo.png'
 import cross_icon from './cross_icon.png'
+import hero_design from './hero_design.jpg'
 
 export const assets = {
     logo,
@@ -93,6 +94,7 @@ export const assets = {
     razorpay_logo,
     stripe_logo,
     cross_icon,
+    hero_design
 }
 
 
---
+++ market_frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero'
+
+const LatestCollection = () => {
+  return (
+    <div>
+      <Hero/>
+      <LatestCollection/>
+    
+    </div>
+  )
+}
+
+export default LatestCollection
