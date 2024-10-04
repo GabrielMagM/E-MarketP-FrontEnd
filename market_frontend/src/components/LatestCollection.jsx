d1c63625f008401a54161a0a711e363cfcb1ba92
Gabriel_MagMed <gabriel.t.13@hotmail.com>
2024-10-04 18:53:54 -0500

    add products

--- market_frontend/src/components/LatestCollection.jsx
+++ market_frontend/src/components/LatestCollection.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react'
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
 import { useEffect } from 'react';
+import ProductItem from './ProductItem';
 const LatestCollection = () => {
     const {products} = useContext(ShopContext);
     const [LatestProducts, setLatestProducts] = useState([]);
@@ -14,12 +15,20 @@ const LatestCollection = () => {
   return (
     <div className='my-10'>
         <div className='text-center py-8 text-3xl'>
-            <Title text1={'LATEST'} text2={'COLLECTION'}/>
+            <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
             At our store, we believe that clothing is more than just fabric – it’s a way to express your personality and style.
             </p>
         </div>
       
+      {/*----------Redering Product Section--------------------------------*/}
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-3'>
+        {
+            LatestProducts.map((item,index)=>(
+              <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+          ))
+          }
+        </div>
 
     </div>
   )
--- market_frontend/src/components/Title.jsx
+++ market_frontend/src/components/Title.jsx
@@ -3,13 +3,11 @@ import React from 'react'
 const Title = ({text1,text2}) => {
   return (
     <div className='inline-flex gap-2 items-center mb-3'>
-        <p className='text-gray-500'>{text1} 
-            <span className='text-gray-700 font-medium'></span>
+        <p className='text-gray-500'>
+            <span className='text-gray-500 font-medium'>{text1} </span>
+            <span className='text-gray-700 font-medium'>{text2}</span>
         </p>
-        <p className='w-8 sm:w-12 has-[1px] sm:has-[2px] bg-gray-700'>
-            
-        </p>
-      
+        <p className='w-8 sm:w-12 h-[1px] sm:h-[2px] bg-gray-700'></p>
     </div>
   )
 }
