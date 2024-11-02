1260015d29cd17fd677e77e27a8cff13f636f875
Gabriel_MagMed <gabriel.t.13@hotmail.com>
2024-11-02 16:23:08 -0500

    Create NewletterBox and export to Home

---
+++ market_frontend/src/components/NewsLetterBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsLetterBox = () => {
+  return (
+    <div className='text-center'>
+        <p className='text-2x1 font-medium text-gray-800'>Suscribe now & get 20% off </p>
+    </div> 
+  )
+}
+
+export default NewsLetterBox
