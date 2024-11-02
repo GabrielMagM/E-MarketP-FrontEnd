db47bdb954d438148b00fd44455806807034c3f4
Gabriel_MagMed <gabriel.t.13@hotmail.com>
2024-11-02 15:42:53 -0500

    Update policy add quiality and exchange images

--- market_frontend/src/components/OurPolicy.jsx
+++ market_frontend/src/components/OurPolicy.jsx
@@ -9,7 +9,22 @@ function OurPolicy() {
             <p className='font-semibold'>Easy Exchange Policy</p>
             <p className='text-gray-400'>We Offer Hassle Free Exchange policy</p>
         </div>
+
+        <div>
+            <img src={assets.quality_icon} className='w-12 m-auto mb-5' alt="" />
+            <p className='font-semibold'>7 Days Return Policy</p>
+            <p className='text-gray-400'>We Provide 7 days free return policy</p>
+        </div>
+
+        <div>
+            <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt="" />
+            <p className='font-semibold'>Best customer support</p>
+            <p className='text-gray-400'>We provide 24/7 Customer Support</p>
+        </div>
+
     </div>
+
+    
   )
 }
 
