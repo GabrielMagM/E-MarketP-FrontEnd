47f42f73ba3623aa2ede248c4494c9d5057cf8fc
Gabriel_MagMed <gabriel.t.13@hotmail.com>
2025-01-02 21:45:48 -0500

    product view 100%

--- market_frontend/src/pages/Product.jsx
+++ market_frontend/src/pages/Product.jsx
@@ -1,12 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
   const {productId} = useParams();
-  const {products} = useContext(ShopContext);
+  const {products, currency} = useContext(ShopContext);
   const [productData,setProductData] = useState(false);
   const [image,setImage] = useState('');
+  const [size,setSize] = useState('');
 
   const fetchProductData = async () =>{
     products.map((item)=>{
@@ -42,11 +45,48 @@ const Product = () => {
           </div>
           {/*-------Product Information------------------- */}
           <div className='flex-1'>
-                <h1>{productData.name}</h1>
+                <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+                <div className='flex items-center gap-1 mt-2'>
+                  <img src={assets.star_icon} alt="" className="w-3 5"/>
+                  <img src={assets.star_icon} alt="" className="w-3 5"/>
+                  <img src={assets.star_icon} alt="" className="w-3 5"/>
+                  <img src={assets.star_icon} alt="" className="w-3 5"/>
+                  <img src={assets.star_dull_icon} alt="" className="w-3 5"/>
+                  <p className='pl-2'>(122)</p>
+                </div>
+                <p className='mt-5 text-2xl font-medium'>{currency}{productData.price}</p>
+                <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+                <div className='flex flex-col gap-4 my-8'>
+                    <p>Select Size </p>
+                    <div className='flex gap-2'>
+                      {productData.sizes.map((item,index)=>(
+                        <button onClick={()=>setSize(item)} key={index} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`}>{item}</button>
+                      ))}
+                    </div>
+                </div>
+                <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+                <div className='text-smtext-gray-500 mt-5 flex flex-col gap-1'>
+                  <p>100% Original product.</p>
+                  <p>Cash on delivery is available on this product.</p>
+                  <p>Easy return and exchange policy within 7 days.</p>
+                </div>
           </div>
-
       </div>
-      
+      {/*-------------Description and Reviews-------------*/}
+      <div className='mt-20'>
+          <div className='flex'>
+              <b className='border px-5 py-3 text-sm'>Description</b>
+              <p className='border px-5 py-3 text-sm'>Reviews(122)</p>
+          </div>
+          <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+              <p>La polo rosa con cuello blanco combina estilo y frescura. Su diseño elegante y versátil es perfecto para cualquier ocasión, desde salidas casuales hasta reuniones informales. Fabricada con materiales suaves y resistentes, es una prenda esencial que aporta un toque de distinción a tu look.</p>
+              <p>Descubre la elegancia y comodidad de nuestra polo rosa con cuello blanco, disponible en nuestra tienda online. Diseñada para quienes buscan un look moderno y versátil, esta prenda es ideal para cualquier ocasión. En nuestro ecommerce encontrarás calidad garantizada y opciones para todos los estilos.</p>
+          </div>
+      </div>
+      {/*-------------Display Related-------------*/}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
+
+
     </div>
   ) : <div className=' opacity-0'></div>
 }
