e7472d09872e7e01921746472cdd3d6243b580e5
Gabriel_MagMed <gabriel.t.13@hotmail.com>
2025-02-13 17:44:45 -0500

    track order

--- market_frontend/src/pages/Orders.jsx
+++ market_frontend/src/pages/Orders.jsx
@@ -17,8 +17,8 @@ const Orders = () => {
         const response = await axios.post(backendUrl + '/api/order/userOrders',{},{headers:{token}})
         console.log(response.data);
         if (response.data.success) {
-          let allOrdersItem = []
-          response.data.Orders.map((order)=>{
+          let allOrdersItem = [];
+          response.data.orders.map((order)=>{
             order.items.map((item)=>{
                 item['status'] = order.status
                 item['payment'] = order.payment
@@ -37,7 +37,7 @@ const Orders = () => {
   }
 
   useEffect(()=>{
-    loadOrderData();
+    loadOrderData()
   },[token])
 
 
@@ -59,15 +59,16 @@ const Orders = () => {
                           <p>Quantity: 1</p>
                           <p>Size: M</p>
                         </div>
-                        <p>Date: <span className='text-gray-400 '>25, JUl, 2024</span></p>
+                        <p>Date: <span className='text-gray-400 '>{new Date(item.date).toDateString()}</span></p>
+                        <p>Date: <span className='text-gray-400 '>{item.paymentMethod}</span></p>
                       </div>
                   </div>
                   <div className='md:w-1/2 flex justify-between'>
                       <div className='flex items-center gap-2'>
                           <p className='min-w-2 h-2 rounded-full bg-green-500'></p>
-                          <p className='text-sm'>Ready to Ship</p>
+                          <p className='text-sm'>{item.status}</p>
                       </div>
-                      <button className='border px-4 py-2 text-sm font-medium rounded-sm'>Track Order</button>
+                      <button onClick={loadOrderData} className='border px-4 py-2 text-sm font-medium rounded-sm'>Track Order</button>
                   </div>
               </div>
             ))
